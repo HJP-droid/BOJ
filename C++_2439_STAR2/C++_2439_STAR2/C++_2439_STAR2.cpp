cc54fabf774f0b01df0e7e2e6d265f45ee47cd4f
JP <JP@DESKTOP-I0E8GQI>
2022-12-24 20:57:04 +0900

    별찍기2

---
+++ C++_2439_STAR2/C++_2439_STAR2/C++_2439_STAR2.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for (int i = 0;i < N;i++)
+    {
+        for (int j = 0;j < N - i - 1;j++)
+            cout << ' ';
+        for (int j = 0;j <= i;j++)
+            cout << '*';
+        cout << endl;
+    }
+
+    return 0;
+}

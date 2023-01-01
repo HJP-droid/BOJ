f104ca44c196c53df6986bd22805d62934e1cc65
JP <JP@DESKTOP-I0E8GQI>
2023-01-02 08:19:44 +0900

    STAR1

--- JAVA_2438_STAR1/src/Main.java
+++ JAVA_2438_STAR1/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Scanner sc = new Scanner(System.in);
+    int num;
+    num = sc.nextInt();
+    for(int count=1 ; count<= num; count++) {
+        for(int i=1 ; i<= count; i++) {
+            System.out.print('*');
+         }
+         System.out.print("\n");
+        }
     }
 }
